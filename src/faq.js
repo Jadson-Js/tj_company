f622469332af731ebff3948206eb717de598286e
jadson-js <jadson20051965@gmail.com>
2025-06-24 10:40:49 -0300

    add photos to FAQ and system to change page

---
+++ src/faq.js
@@ -0,0 +1,60 @@
+let currentPage = 0;
+
+const nextCards = document.getElementById("nextCards");
+const previousCards = document.getElementById("previousCards");
+const pageBar = document.getElementById("pageBar");
+const card0 = document.getElementById("card_0");
+const card1 = document.getElementById("card_1");
+const card2 = document.getElementById("card_2");
+const card3 = document.getElementById("card_3");
+
+previousCards.addEventListener("click", () => {
+  changePage(-1);
+
+  changeCardsBg();
+  changePageBar();
+});
+
+nextCards.addEventListener("click", () => {
+  changePage(1);
+
+  changeCardsBg();
+  changePageBar();
+});
+
+function changePage(page) {
+  if (currentPage + page < 0 || currentPage + page > 4) return;
+
+  currentPage += page;
+}
+
+function changeCardsBg() {
+  card0.style.cssText = `
+    background: url('/public/photos/photo_${
+      currentPage * 4
+    }.jpg') center / cover no-repeat;
+  `;
+
+  card1.style.cssText = `
+    background: url('/public/photos/photo_${
+      1 + currentPage * 4
+    }.jpg') center / cover no-repeat;
+  `;
+
+  card2.style.cssText = `
+    background: url('/public/photos/photo_${
+      2 + currentPage * 4
+    }.jpg') center / cover no-repeat;
+  `;
+
+  card3.style.cssText = `
+    background: url('/public/photos/photo_${
+      3 + currentPage * 4
+    }.jpg') center / cover no-repeat;
+  `;
+  console.log(3 + currentPage * 4);
+}
+
+function changePageBar() {
+  pageBar.style.width = `${currentPage * 25}%`;
+}
---
+++ src/main.js
@@ -0,0 +1,47 @@
+document.querySelectorAll(".form").forEach((form) => {
+  const input = form.querySelector(".message");
+
+  input.addEventListener("input", () => {
+    let val = input.value.replace(/\D/g, "");
+
+    if (val.length > 11) val = val.slice(0, 11); // Corrigido aqui
+
+    const ddd = val.slice(0, 2);
+    const prefixo = val.slice(2, 7);
+    const sufixo = val.slice(7, 11);
+
+    if (val.length >= 7) {
+      input.value = `(${ddd}) ${prefixo}-${sufixo}`;
+    } else if (val.length >= 3) {
+      input.value = `(${ddd}) ${val.slice(2)}`;
+    } else {
+      input.value = `(${val}`;
+    }
+  });
+
+  form.addEventListener("submit", (e) => {
+    if (input.length !== 11) {
+      e.preventDefault();
+      alert("Digite um número de WhatsApp válido com DDD.");
+    }
+  });
+});
+
+/* ----------------------------------------------------------- */
+
+let currentFaq = null;
+const faqUl = document.getElementById("faq");
+
+faqUl.querySelectorAll(".faq__li").forEach((li) => {
+  const details = li.querySelector("details");
+
+  details.addEventListener("toggle", () => {
+    if (details.open) {
+      faqUl.querySelectorAll("details").forEach((other) => {
+        if (other !== details) other.open = false;
+      });
+
+      currentFaq = details.id;
+    }
+  });
+});
